f6bd427043a5c5f76e5fec1656b14e1c643e0c0b
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2022-08-13 13:44:20 -0400

    feat: new modify color system

---
+++ src-tauri/src/cmd.rs
@@ -0,0 +1,64 @@
+use std::{str::FromStr, time::Duration};
+
+use clap::{Parser, Subcommand};
+use humantime::parse_duration;
+
+const DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
+const AUTHORS: &str = env!("CARGO_PKG_AUTHORS");
+
+#[derive(Parser, Default, Debug)]
+#[clap(author = AUTHORS, version, about=DESCRIPTION)]
+pub struct Arguments {
+    #[clap(short, long)]
+    pub no_gui: bool,
+    #[clap(short, long)]
+    pub show_devices: bool,
+    #[clap(short, long)]
+    pub color: Option<HexData>,
+    #[clap(short, long, parse(try_from_str = parse_duration))]
+    pub time: Option<Duration>,
+    #[clap(subcommand)]
+    pub type_color: Option<TypeColor>,
+}
+
+#[derive(Clone, PartialEq, Debug)]
+pub struct HexData(Vec<u8>);
+
+impl HexData {
+    pub fn get_data(self) -> Vec<u8> {
+        self.0
+    }
+}
+
+impl std::str::FromStr for HexData {
+    type Err = hex::FromHexError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        hex::decode(s).map(HexData)
+    }
+}
+
+#[derive(Subcommand, Default, Debug, Clone)]
+pub enum TypeColor {
+    #[default]
+    Full,
+    Row {
+        index: u8
+    },
+    Single {
+        index: u8
+    },
+}
+
+impl FromStr for TypeColor {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_ascii_lowercase().as_str() {
+            "full" => Ok(TypeColor::Full),
+            "row" => Ok(TypeColor::Full),
+            "single" => Ok(TypeColor::Full),
+            _ => Err(clap::ErrorKind::InvalidValue.to_string())
+        }
+    }
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,52 +2,23 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
+use std::{thread, time::Duration};
 
-use clap::{Arg, Command};
+use clap::Parser;
+use humantime::parse_duration;
 
+mod cmd;
 mod hid;
+use cmd::{Arguments, TypeColor};
 use hid::{
-    color::{hex_to_rgb, send_color, RgblightType},
-    is_my_device,
+    color::{get_current_color, send_color},
+    is_my_device, VENDOR_ID, PRODUCT_ID,
 };
-use hidapi::HidApi;
-
-use crate::hid::{PRODUCT_ID, USAGE_ID, USAGE_PAGE, VENDOR_ID};
-
-const VERSION: &str = env!("CARGO_PKG_VERSION");
-const DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
-const AUTHORS: &str = env!("CARGO_PKG_AUTHORS");
+use hidapi_rusb::{HidApi, HidDevice};
 
 fn main() {
-    let matches = Command::new("Crkbd Manager")
-        .about(DESCRIPTION)
-        .version(VERSION)
-        .author(AUTHORS)
-        .args(&[
-            Arg::new("NoGui")
-                .long("no-gui")
-                .help("Excecute in batch mode")
-                .takes_value(false),
-            Arg::new("Color")
-                .long("color")
-                .short('c')
-                .help("Change all color of the keyboard (this use the HEX color, example #9ad0ff)")
-                .takes_value(true),
-            Arg::new("Restore")
-                .long("restore")
-                .short('r')
-                .help("Restore keyboard color in time")
-                .takes_value(true),
-            Arg::new("Device_List")
-                .long("list")
-                .short('l')
-                .help("Show list Devices")
-                .takes_value(false),
-        ])
-        .get_matches();
-
-    let show_devices = matches.is_present("Device_List");
-    if !matches.is_present("NoGui") {
+    let args = Arguments::parse();
+    if !args.no_gui {
         tauri::Builder::default()
             .run(tauri::generate_context!())
             .expect("error while running tauri application");
@@ -58,13 +29,15 @@ fn main() {
         std::process::exit(1);
     });
 
-    let device = api
+    let devices: Vec<&hidapi_rusb::DeviceInfo> = api
         .device_list()
-        .find(|device| {
-            if show_devices {
+        .filter(|device| {
+            if args.show_devices {
                 println!(
-                    "{}, {}, {}, {}, {}",
+                    "{} - {}, {:#?}, {}, {}, {}, {}",
+                    device.manufacturer_string().unwrap_or_default(),
                     device.product_string().unwrap_or_default(),
+                    device.path(),
                     device.vendor_id(),
                     device.product_id(),
                     device.usage_page(),
@@ -73,18 +46,48 @@ fn main() {
             }
             is_my_device(device)
         })
-        .unwrap_or_else(|| {
-            eprintln!("Could not find keyboard");
-            std::process::exit(1);
-        })
-        .open_device(&api)
-        .unwrap_or_else(|_| {
-            eprintln!("Could not open HID device");
-            std::process::exit(1);
-        });
+        .collect();
 
-    if let Some(hex) = matches.value_of("Color") {
-        let color = hex_to_rgb(hex).unwrap();
-        send_color(&device, color, RgblightType::Full);
+    let time = args.time.unwrap_or(parse_duration("10s").unwrap());
+
+    if let Some(raw) = args.color {
+        let color = raw.get_data();
+        if devices.len() == 0 {
+            eprintln!("Could not find keyboard");
+        } else {
+            for d in devices {
+                let type_color = args.type_color.clone();
+                let device = d.open_device(&api).expect(
+                    format!(
+                        "{}: {}",
+                        "Could not open HID device",
+                        d.product_string().unwrap_or("")
+                    )
+                    .as_str(),
+                );
+                // let device = &api.open(VENDOR_ID, PRODUCT_ID).unwrap();
+                let color = color.clone();
+                change_color_and_restore(&device, color, time.clone(), type_color);
+            }
+        }
+    }
+}
+
+fn change_color_and_restore(
+    device: &HidDevice,
+    color: Vec<u8>,
+    delay: Duration,
+    type_color: Option<TypeColor>,
+) {
+    let last_color = get_current_color(&device, 500).unwrap();
+    println!("Data Received: {:?}", last_color);
+    match send_color(&device, color.clone(), type_color.clone()) {
+        Ok(w) => println!("Data Sended: {w}"),
+        Err(e) => println!("Fail to send data: {e}"),
+    }
+    thread::sleep(delay);
+    match send_color(&device, last_color.0.clone(), type_color) {
+        Ok(w) => println!("Data Sended: {w}"),
+        Err(e) => println!("Fail to send data: {e}"),
     }
 }
